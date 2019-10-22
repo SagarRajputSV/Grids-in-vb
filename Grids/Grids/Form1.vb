5109c298edbbc880063fe4a7d689aec61884164e
Sagar R Rajput <sagartubearn@gmail.com>
2019-10-22 16:51:26 +0530

    Initial Commit

---
+++ Grids/Grids/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+
+        Dim br0 As New SolidBrush(Color.CadetBlue)
+
+        Dim rt As New Rectangle(10, 100, 100, 20)
+
+        'e.Graphics.DrawRectangle(Pens.Red, rt)
+        e.Graphics.FillRectangle(br0, rt)
+    End Sub
+
+
+End Class
